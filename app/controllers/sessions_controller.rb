fbd77bdc89bdebfe72d530570b7a4006c87b98ee
Drew MacNicholas <dmacnicholas@gmail.com>
2022-09-15 17:33:27 -0500

    Update routes for redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(request.env['omniauth.auth'])
       if user.valid?
         session[:user_id] = user.id
-        redirect_to user_path(user)
+        redirect_to dashboard_url
     else
       redirect_to root
     end
