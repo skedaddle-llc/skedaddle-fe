c3b59b73e5fd50134062c6e0dac8c8d71c30c5fe
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-15 20:46:19 -0400

    add passing test login

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -6,8 +6,24 @@ RSpec.describe 'Welcome Page' do
 
     expect(page).to have_button('Log in with Google')
 
+    OmniAuth.config.test_mode = true
+	  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+        {"provider" => "google_oauth2",
+        "uid" => "123456",
+        "info" => {
+        "name" => "John Doe",
+        "email" => "johndoe@example.com",
+        },
+        "credentials" => {
+        "token" => "TOKEN",
+        },
+        }) 
+
     click_button('Log in with Google')
 
+    expect(current_path).to eql('/dashboard')
+    expect(User.last.uid).to eq("123456")
+    expect(User.last.email).to eq("johndoe@example.com")
 
   end
 end
