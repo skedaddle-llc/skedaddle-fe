230f13532bc4e9228ca45db0f7abb97ddbccd869
Eli Sachs <easachs13@gmail.com>
2022-09-17 21:19:14 -0600

    Refactor: improve new itinerary

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,21 +1,22 @@
 class ItinerariesController < ApplicationController
 
   def new
+    @itinerary = Itinerary.new
     @search = itinerary_params[:search]
     @parks = ParksService.parks_near(@search)
     @restaurants = RestaurantsService.restaurants_near(@search)
   end
 
-  # def create
-  #   itinerary = Itinerary.new(itinerary_params)
-  #   if itinerary.save
-  #     flash[:success] = "New itinerary saved."
-  #     redirect_to itinerary_path(itinerary.id)
-  #   else
-  #     flash[:error] = itinerary.errors.full_messages.uniq * ', '
-  #     redirect_to root
-  #   end
-  # end
+  def create
+    itinerary = Itinerary.new(itinerary_params)
+    if itinerary.save
+      flash[:success] = "New itinerary saved."
+      redirect_to itinerary_path(itinerary.id)
+    else
+      flash[:error] = itinerary.errors.full_messages.uniq * ', '
+      redirect_to root_path
+    end
+  end
 
   # def show
   #   @itinerary = find_itinerary
