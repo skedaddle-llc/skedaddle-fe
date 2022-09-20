6d8fb45f0efd7636e24f9051e8a5a95b0eaad9be
Nick Jones <nickjones9605@gmail.com>
2022-09-19 20:43:02 -0600

    Adds about us controller and route. Adds About Us html.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#omniauth'
   get '/dashboard', to: 'dashboard#show'
   delete '/sessions', to: 'sessions#destroy'
+  get '/about', to: 'about_us_contact#about_us'
 
   resources :itineraries, except: %i[edit update]
 end
