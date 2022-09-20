7760f4f73b999e6781a13e096869632a33f6c480
Nick Jones <nickjones9605@gmail.com>
2022-09-19 20:56:55 -0600

    Adds contact route and acontact controller function to about us contact controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'dashboard#show'
   delete '/sessions', to: 'sessions#destroy'
   get '/about', to: 'about_us_contact#about_us'
+  get '/contact', to: 'about_us_contact#contact'
 
   resources :itineraries, except: %i[edit update]
 end
