6e4b546ba648daa929767d60999ac26e09d33136
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-20 22:53:46 -0400

    update welcome page skedaddle naming

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,56 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_919_053_343) do
+ActiveRecord::Schema.define(version: 2022_09_19_053343) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'itineraries', force: :cascade do |t|
-    t.string 'search'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_itineraries_on_user_id'
+  create_table "itineraries", force: :cascade do |t|
+    t.string "search"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_itineraries_on_user_id"
   end
 
-  create_table 'parks', force: :cascade do |t|
-    t.string 'name'
-    t.string 'city'
-    t.string 'state'
-    t.string 'country'
-    t.string 'description'
-    t.string 'directions'
-    t.string 'lat'
-    t.string 'lon'
-    t.string 'activities'
-    t.string 'url'
-    t.string 'thumbnail'
-    t.bigint 'itinerary_id'
-    t.index ['itinerary_id'], name: 'index_parks_on_itinerary_id'
+  create_table "parks", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "description"
+    t.string "directions"
+    t.string "lat"
+    t.string "lon"
+    t.string "activities"
+    t.string "url"
+    t.string "thumbnail"
+    t.bigint "itinerary_id"
+    t.index ["itinerary_id"], name: "index_parks_on_itinerary_id"
   end
 
-  create_table 'restaurants', force: :cascade do |t|
-    t.string 'name'
-    t.string 'rating'
-    t.string 'price'
-    t.string 'image_url'
-    t.string 'url'
-    t.string 'categories'
-    t.string 'address'
-    t.string 'phone'
-    t.bigint 'itinerary_id'
-    t.index ['itinerary_id'], name: 'index_restaurants_on_itinerary_id'
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "rating"
+    t.string "price"
+    t.string "image_url"
+    t.string "url"
+    t.string "categories"
+    t.string "address"
+    t.string "phone"
+    t.bigint "itinerary_id"
+    t.index ["itinerary_id"], name: "index_restaurants_on_itinerary_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'uid'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
-  add_foreign_key 'itineraries', 'users'
-  add_foreign_key 'parks', 'itineraries'
-  add_foreign_key 'restaurants', 'itineraries'
+  add_foreign_key "itineraries", "users"
+  add_foreign_key "parks", "itineraries"
+  add_foreign_key "restaurants", "itineraries"
 end
